2da2d0270189c6e8a84b013bfac7e08a5ba0f6a7
Ori Geva <origeva636@gmail.com>
2021-01-09 22:10:39 +0200

    Shorthanded an expression

--- test/tests/getSegmentsTest.ts
+++ test/tests/getSegmentsTest.ts
@@ -3,7 +3,7 @@ import { strict as assert } from 'assert';
 import { isSegment } from '../../src/types/segment.model';
 
 export async function getSegmentsTest(sponsorBlock?: SponsorBlock): Promise<void> {
-	sponsorBlock = sponsorBlock ? sponsorBlock : SponsorBlock.newUser();
+	sponsorBlock = sponsorBlock || SponsorBlock.newUser();
 	let segments = await sponsorBlock.getSegments('jiK2jmTVF3A');
 	assert.ok(segments.every(isSegment), `sponsorBlock.getSegments('jiK2jmTVF3A'): segments is not of type Segment[]`);
 	assert.equal(segments[0].category, 'sponsor', `sponsorBlock.getSegments('jiK2jmTVF3A'): Default category from API is not "sponsor"`);
--- test/tests/isUserVIPTest.ts
+++ test/tests/isUserVIPTest.ts
@@ -2,7 +2,7 @@ import SponsorBlock from '../../src/index';
 import { strict as assert } from 'assert';
 
 export async function isUserVIPTest(sponsorBlock?: SponsorBlock): Promise<void> {
-	sponsorBlock = sponsorBlock ? sponsorBlock : SponsorBlock.newUser();
+	sponsorBlock = sponsorBlock || SponsorBlock.newUser();
 	var response = await sponsorBlock.isUserVIP();
 	assert.ok(response.hashedUserID, 'isUserVIPTest(sponsorBlock?: SponsorBlock): hashedUserID is falsy');
 	assert.notEqual(response.vip, undefined, 'isUserVIPTest(sponsorBlock?: SponsorBlock): vip is undefined');
