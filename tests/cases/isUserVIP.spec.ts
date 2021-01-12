86d4e1f3d05bf9fab2468f6e98d12095abd3ed99
Ori Geva <origeva636@gmail.com>
2021-01-12 23:59:16 +0200

    Added subtest, change equal to strictEqual

--- tests/cases/isUserVIP.spec.ts
+++ tests/cases/isUserVIP.spec.ts
@@ -7,6 +7,10 @@ const { sponsorBlock } = config;
 describe('isUserVIP', () => {
 	it('should not return undefined', async () => {
 		let response = await sponsorBlock.isUserVIP();
-		assert.notEqual(response.vip, undefined, 'isUserVIP(): vip is undefined');
+		assert.notStrictEqual(response.vip, undefined, 'isUserVIP(): vip is undefined');
+	});
+	it('should return false', async () => {
+		let response = await sponsorBlock.isUserVIP();
+		assert.strictEqual(response.vip, false, 'isUserVIP(): vip is undefined');
 	});
 });
