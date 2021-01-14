22d950f6764f0c43a15510eef8d4ac60f9864d70
Ori Geva <origeva636@gmail.com>
2021-01-14 13:22:11 +0200

    Removed DBVideo

--- src/types/video.model.ts
+++ src/types/video.model.ts
@@ -1,14 +1,3 @@
-import { DBSegment, dbsegmentToSegment, Segment } from './segment.model';
+import { Segment } from './segment.model';
 
 export type Video = { videoID: string; hash: string; segments: Segment[] };
-
-export type DBVideo = { videoID: string; hash: string; segments: DBSegment[] };
-
-export function dbvideoToVideo(dbvideo: DBVideo): Video {
-	let { videoID, hash, segments } = dbvideo;
-	return { videoID, hash, segments: segments.map(dbsegmentToSegment) };
-}
-
-// export default class Video {
-// 	constructor(public videoID: string, public hash: string, public segments: Segment[]) {}
-// }
