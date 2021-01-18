dc0eefce8375c00a04b2ed63dbfb1dcd59f6d645
Ori Geva <origeva636@gmail.com>
2021-01-18 14:08:40 +0200

    Fixed conflict

--- tests/cases/getSegmentsPrivately.spec.ts
+++ tests/cases/getSegmentsPrivately.spec.ts
@@ -5,12 +5,8 @@ import { config } from '../config';
 const { sponsorBlock, videoID } = config;
 
 describe('getSegmentsPrivately', () => {
-<<<<<<< HEAD
 	it('should return a Video', async function () {
 		this.timeout(3000);
-=======
-	it('should return a video with segments of type Segment', async () => {
->>>>>>> f6a2195c4b43160f2e1fbfbcc0b59d8b3e1e8afa
 		let video = await sponsorBlock.getSegmentsPrivately(videoID);
 		assert.ok(video.segments.every(isSegment), `sponsorBlock.getSegmentsPrivately('${videoID}'): segments is not of type Segment[]`);
 	});
