a6bc88aeffa5ed049422af09d5286f91c09e2a85
Ori Geva <origeva636@gmail.com>
2021-01-19 14:05:12 +0200

    Improved extractVideoURL regex, added validateYoutubeURL function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+const regex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+
 /**
  * Extracts the video ID from the full URL.
  * this function assumes the input is surely a YouTube video URL, otherwise may return null or a part of the input.
@@ -5,7 +7,10 @@
  * @returns The video ID extracted from the input URL.
  */
 export function extractVideoID(youtubeURL: string): string | null {
-	let regex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
 	let matchArray = youtubeURL.match(regex);
-	return matchArray && matchArray[2];
+	return matchArray && matchArray[5];
+}
+
+export function validateYoutubeURL(youtubeURL: string): boolean {
+	return regex.test(youtubeURL);
 }
