31742b9f9e148c9c3f61a65383f906bd9dfbf251
Ori Geva <origeva636@gmail.com>
2021-01-19 15:21:55 +0200

    Changed to expect segments (instead of video)

--- tests/cases/getSegmentsPrivately.spec.ts
+++ tests/cases/getSegmentsPrivately.spec.ts
@@ -6,8 +6,8 @@ const { sponsorBlock, videoID } = config;
 
 describe('getSegmentsPrivately', () => {
 	it('should return a Video', async () => {
-		let video = await sponsorBlock.getSegmentsPrivately(videoID);
-		assert.ok(video.segments.every(isSegment), `sponsorBlock.getSegmentsPrivately('${videoID}'): segments is not of type Segment[]`);
+		let segments = await sponsorBlock.getSegmentsPrivately(videoID);
+		assert.ok(segments.every(isSegment), `sponsorBlock.getSegmentsPrivately('${videoID}'): segments is not of type Segment[]`);
 	});
 	// assert.equal(video.segments[0].category, 'sponsor', `sponsorBlock.getSegmentsPrivately('${videoID}'): Default category from API is not "sponsor"`);
 	// video = await sponsorBlock.getSegmentsPrivately(videoID, 'intro', 'outro');
