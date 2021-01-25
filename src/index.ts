6d843254cdcc9314eadd3206e9815ceeb079930e
Ori Geva <origeva636@gmail.com>
2021-01-25 02:25:04 +0200

    Removed unnecessary import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { SponsorBlockOptions } from './apis/interfaces';
-import { PrefixRange } from './types/PrefixRange';
 
 export const config: SponsorBlockOptions = {
 	baseURL: 'https://sponsor.ajay.app', // Base URL for the api endpoints
@@ -10,4 +9,3 @@ export { SponsorBlock, SponsorBlockVIP, SponsorBlockAdmin } from './apis/first';
 export { SponsorBlockLegacy } from './apis/legacy';
 export { validateYoutubeURL, extractVideoID } from './utils';
 export { ResponseError } from './apis/ResponseError';
-// export { Category } from './types/segment/Category';
