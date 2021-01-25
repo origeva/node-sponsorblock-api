580692b1cf020df8177a54fbe43d7319001f925c
Ori Geva <origeva636@gmail.com>
2021-01-25 02:30:47 +0200

    refactored config to defaultOptions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { SponsorBlockOptions } from './apis/interfaces';
 
-export const config: SponsorBlockOptions = {
+export const defaultOptions: SponsorBlockOptions = {
 	baseURL: 'https://sponsor.ajay.app', // Base URL for the api endpoints
 	hashPrefixLength: 4, // Recommended prefix length to use for getting segments privately, to balance between privacy and more accurate results
 };
