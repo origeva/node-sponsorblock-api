4becb5cfa7b097eaf4dfc95c6bf7bf9bd39c2208
Ori Geva <origeva636@gmail.com>
2021-01-29 04:00:04 +0200

    Response errors now start with "[SponsorBlock]"

--- src/apis/utils.ts
+++ src/apis/utils.ts
@@ -4,19 +4,19 @@ import { ResponseError } from './ResponseError';
 export function statusCheck(res: Response) {
 	if (res.status !== 200) {
 		if (res.status === 400) {
-			throw new ResponseError(400, 'Bad Request (Your inputs are wrong/impossible)');
+			throw new ResponseError(400, '[SponsorBlock] Bad Request (Your inputs are wrong/impossible)');
 		} else if (res.status == 403) {
-			throw new ResponseError(403, `Rejected by auto moderator`);
+			throw new ResponseError(403, `[SponsorBlock] Rejected by auto moderator`);
 		} else if (res.status === 404) {
-			throw new ResponseError(404, 'Not Found');
+			throw new ResponseError(404, '[SponsorBlock] Not Found');
 		} else if (res.status === 405) {
-			throw new ResponseError(405, 'Duplicate');
+			throw new ResponseError(405, '[SponsorBlock] Duplicate');
 		} else if (res.status === 409) {
-			throw new ResponseError(409, 'Duplicate');
+			throw new ResponseError(409, '[SponsorBlock] Duplicate');
 		} else if (res.status === 429) {
-			throw new ResponseError(429, 'Rate Limit (Too many for the same user or IP)');
+			throw new ResponseError(429, '[SponsorBlock] Rate Limit (Too many for the same user or IP)');
 		} else {
-			throw new ResponseError(res.status, `Status code not 200 (${res.status})`);
+			throw new ResponseError(res.status, `[SponsorBlock] Status code not 200 (${res.status})`);
 		}
 	}
 }
