6f16e276af9196b8228c4cc27146209258c232dd
Ori Geva <origeva636@gmail.com>
2021-07-14 04:16:10 +0300

    Added rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  // bracketSpacing: false,
+  arrowParens: 'always',
+  endOfLine: 'crlf',
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  useTabs: true,
+  tabWidth: 2,
+  bracketSpacing: true,
   singleQuote: true,
-  // trailingComma: 'all',
+  trailingComma: 'es5',
   printWidth: 180,
 };
