31e9296ec0411d71cf538a87009771b2cb052e48
Ori Geva <origeva636@gmail.com>
2023-03-16 00:44:48 +0200

    Added filler category and link to category breakdown

--- src/types/segment/Category.ts
+++ src/types/segment/Category.ts
@@ -1,5 +1,7 @@
 /**
  * The category of a segment
+ * 
+ * {@link https://wiki.sponsor.ajay.app/w/Guidelines#Category_Breakdown Category Breakdown}
  */
 
-export type Category = 'sponsor' | 'intro' | 'outro' | 'interaction' | 'selfpromo' | 'music_offtopic' | 'preview';
+export type Category = 'sponsor' | 'intro' | 'outro' | 'interaction' | 'selfpromo' | 'music_offtopic' | 'preview' | 'filler';
