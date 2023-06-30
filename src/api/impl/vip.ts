8c7006ac699c546431eabcf620c21a194b017a73
Ori Geva <origeva636@gmail.com>
2023-06-30 20:43:11 +0300

    Using axios instance with default options

--- src/api/impl/vip.ts
+++ src/api/impl/vip.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { Category } from '../../types/segment/Category';
 import { SponsorBlockOptions } from '../../types/SponsorBlockOptions';
 import { SponsorBlock } from './user';
@@ -14,38 +13,38 @@ export class SponsorBlockVIP extends SponsorBlock implements SponsorBlockVIPInte
 
 	async blockSubmissionsOfCategory(video: VideoResolvable, ...categories: Category[]): Promise<void> {
 		let videoID = resolveVideo(video);
-		let res = await axios.post('/api/noSegments', { videoID, userID: this.userID, categories }, { baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.post('/api/noSegments', { videoID, userID: this.userID, categories })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
 
 	async shadowBan(publicUserID: string, hideOldSubmissions?: boolean): Promise<void> {
-		let res = await axios.post('/api/shadowBanUser', null, { params: { userID: publicUserID, adminUserID: this.userID, unHideOldSubmissions: hideOldSubmissions ?? false }, baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.post('/api/shadowBanUser', null, { params: { userID: publicUserID, adminUserID: this.userID, unHideOldSubmissions: hideOldSubmissions ?? false } })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
 
 	async removeShadowBan(publicUserID: string): Promise<void> {
-		let res = await axios.post('/api/shadowBanUser', null, { params: { userID: publicUserID, adminUserID: this.userID, enabled: false }, baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.post('/api/shadowBanUser', null, { params: { userID: publicUserID, adminUserID: this.userID, enabled: false } })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
 
 	async hideOldSubmissions(publicUserID: string): Promise<void> {
-		let res = await axios.post('/api/shadowBanUser' , null, { params: { userID: publicUserID, adminUserID: this.userID, enabled: true, unHideOldSubmissions: true }, baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.post('/api/shadowBanUser' , null, { params: { userID: publicUserID, adminUserID: this.userID, enabled: true, unHideOldSubmissions: true } })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
 
 	async warnUser(publicUserID: string, reason: string = '', enabled?: boolean): Promise<void> {
-		let res = await axios.post('/api/warnUser', { issuerUserID: this.userID, userID: publicUserID, enabled, reason }, { baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.post('/api/warnUser', { issuerUserID: this.userID, userID: publicUserID, enabled, reason })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
 
 	async clearCache(video: VideoResolvable): Promise<void> {
 		let videoID = resolveVideo(video);
-		let res = await axios.get('/api/clearCache?videoID', { params: { videoID, userID: this.userID }, baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.get('/api/clearCache?videoID', { params: { videoID, userID: this.userID } })
 		// let res = await fetch(`${this.options.baseURL}/api/clearCache?videoID=${videoID}&userID=${this.userID}`);
 		statusCheck(res);
 		// returns nothing (status code 200)
@@ -53,7 +52,7 @@ export class SponsorBlockVIP extends SponsorBlock implements SponsorBlockVIPInte
 
 	async purgeAllSegments(video: VideoResolvable): Promise<void> {
 		let videoID = resolveVideo(video);
-		let res = await axios.get('/api/purgeAllSegments', { params: { videoID, userID: this.userID }, baseURL: this.options.baseURL, validateStatus: null })
+		let res = await this.http.get('/api/purgeAllSegments', { params: { videoID, userID: this.userID } })
 		statusCheck(res);
 		// returns nothing (status code 200)
 	}
