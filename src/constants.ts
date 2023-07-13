b08a94c9f030f4b1a2fe30efc12649f97760c9f1
Ori Geva <origeva636@gmail.com>
2023-07-13 15:48:55 +0300

    typeof to infer Category type from ALL_CATEGORIES

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,5 @@
-import { Category } from "./types";
-
 export class Constants {
 
-    static readonly ALL_CATEGORIES: Category[] = ['sponsor', 'intro', 'outro', 'interaction', 'selfpromo', 'music_offtopic', 'preview', 'filler'];
+    static readonly ALL_CATEGORIES = ['sponsor', 'intro', 'outro', 'interaction', 'selfpromo', 'music_offtopic', 'preview', 'filler'] as const;
 
 }
--- src/types/segment/Category.ts
+++ src/types/segment/Category.ts
@@ -1,7 +1,8 @@
+import { Constants } from "src/constants";
+
 /**
  * The category of a segment
  * 
  * {@link https://wiki.sponsor.ajay.app/w/Guidelines#Category_Breakdown Category Breakdown}
  */
-
-export type Category = 'sponsor' | 'intro' | 'outro' | 'interaction' | 'selfpromo' | 'music_offtopic' | 'preview' | 'filler';
+export type Category = typeof Constants.ALL_CATEGORIES;
